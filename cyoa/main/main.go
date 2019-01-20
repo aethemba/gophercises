f02dadffde1eecf87c2c92185cadf6f3e566b8f7
Aksel Ethem <aksel.ethem@gmail.com>
2019-01-20 19:13:22 +0100

    Initial implementation of Choose your own adventure

---
+++ cyoa/main/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type Story map[string]Arc
+
+type Option struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
+
+type Arc struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []Option `json:"options"`
+}
+
+var s Story
+
+func LoadFile(filename string) []byte {
+	f, err := os.Open(filename)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	return data
+}
+
+func LoadJSONStory(data []byte) Story {
+
+	var result Story
+	err := json.Unmarshal(data, &result)
+
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
+	return result
+}
+
+func main() {
+
+	data := LoadFile("story.json")
+	s = LoadJSONStory(data)
+
+	http.HandleFunc("/", welcomeHandler)
+	http.HandleFunc("/arc/", arcHandler)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func arcHandler(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("arc.html"))
+
+	path := r.URL.Path
+
+	key := strings.SplitAfter(path, "/arc/")
+
+	data := struct {
+		Title   string
+		Story   string
+		Options []Option
+	}{
+		Title: "Arc page",
+	}
+
+	if v, ok := s[key[1]]; ok {
+		data.Story = v.Story[0]
+		data.Options = v.Options
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Story not found")
+		return
+	}
+
+	t.Execute(w, data)
+}
+
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("welcome.html"))
+
+	data := struct {
+		Title string
+	}{
+		Title: "Welcome page",
+	}
+
+	t.Execute(w, data)
+}
