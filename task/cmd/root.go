7bca2c2201ef2f4a86816c0a882c17a65968e99b
Aksel Ethem <aksel.ethem@gmail.com>
2019-01-23 16:19:31 +0100

    Added initial task-cli shell

---
+++ task/cmd/root.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "task",
+	Short: "Task-cli is a fast and simple tool for maintaning todo items",
+	Long:  "Do we really need to make this long?",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Running")
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return errors.New("\nPlease provide a command, e.g. add or list\n")
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+// func init() {
+// 	cobra.OnInitialize(initConfig)
+
+// }
+
+// func initConfig() {
+// 	if cfgFile != "" {
+// 		viper.SetConfigFile(cfgFile)
+// 	} else {
+// 		home, err := homedir.Dir()
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			os.Exit(1)
+// 		}
+// 	}
+// }
---
+++ task/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"gophercises/task/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
