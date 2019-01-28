741a0d4bd36c51f98090a6e58c0f37026c60e5b3
Aksel Ethem <aksel.ethem@gmail.com>
2019-01-28 15:16:05 +0100

    Working, reafactored

--- deck/main/main.go
+++ deck/main/main.go
@@ -41,6 +41,7 @@ func clone(gs GameState) GameState {
 		Player: make(Hand, len(gs.Player)),
 		Dealer: make(Hand, len(gs.Dealer)),
 		Turn:   gs.Turn,
+		State:  gs.State,
 	}
 	copy(ret.Deck, gs.Deck)
 	copy(ret.Player, gs.Player)
@@ -94,57 +95,110 @@ func draw(cards []deck.Card) (deck.Card, []deck.Card) {
 	return cards[0], cards[1:]
 }
 
+func Shuffle(gs GameState) GameState {
+	ret := clone(gs)
+
+	cards := deck.New(deck.Deck(3))
+	ret.Deck = deck.Shuffle(cards)
+	return ret
+}
+
+func Deal(gs GameState) GameState {
+	ret := clone(gs)
+
+	ret.Player = make(Hand, 0, 5)
+	ret.Dealer = make(Hand, 0, 5)
+
+	var card deck.Card
+	for i := 0; i < 2; i++ {
+		card, ret.Deck = draw(ret.Deck)
+		ret.Player = append(ret.Player, card)
+
+		card, ret.Deck = draw(ret.Deck)
+		ret.Dealer = append(ret.Dealer, card)
+	}
+
+	ret.State = StatePlayerTurn
+
+	return ret
+}
+
+func Hit(gs GameState) GameState {
+	ret := clone(gs)
+
+	hand := ret.CurrentPlayer()
+	fmt.Println("Hit", hand)
+	var card deck.Card
+	card, ret.Deck = draw(ret.Deck)
+	*hand = append(*hand, card)
+
+	return ret
+}
+
+func Stand(gs GameState) GameState {
+	ret := clone(gs)
+	ret.State++
+	return ret
+}
+
+func EndHand(gs GameState) GameState {
+	ret := clone(gs)
+
+	pScore, dScore := ret.Player.Score(), ret.Dealer.Score()
+
+	fmt.Println("**Final hands**")
+	fmt.Println("Player: ", ret.Player, "\nScore: ", pScore)
+	fmt.Println("Dealer: ", ret.Dealer, "\nScore: ", dScore)
+
+	switch {
+	case pScore > 21:
+		fmt.Println("You busted")
+	case dScore > 21:
+		fmt.Println("Dealer busted")
+	case pScore > dScore:
+		fmt.Println("You win!")
+	case dScore > pScore:
+		fmt.Println("You lose...")
+	case dScore == pScore:
+		fmt.Println("Draw!")
+	}
+
+	ret.Player = nil
+	ret.Dealer = nil
+	return ret
+}
+
 func main() {
 	var gs GameState
-	gs.Deck = deck.New(deck.Deck(3))
-	// fmt.Println("Playing blackjack")
-	// cards := deck.New(deck.Deck(3))
-	// cards = deck.Shuffle(cards)
-	// var card deck.Card
+	gs = Shuffle(gs)
 
-	// var player, dealer Hand
-	// for i := 0; i < 2; i++ {
-	// 	for _, h := range []*Hand{&player, &dealer} {
-	// 		card, cards = draw(cards)
-	// 		*h = append(*h, card)
-	// 	}
-	// }
+	gs = Deal(gs)
 
-	// var input string
-	// for input != "s" {
-	// 	fmt.Println("Hand player: ", player)
-	// 	fmt.Println("Hand dealer: ", dealer.DealerString())
-	// 	fmt.Println("Action? (H)it or (S)tand?")
-	// 	fmt.Scanf("%s\n", &input)
+	var input string
+	for gs.State == StatePlayerTurn {
+		fmt.Println("Hand player: ", gs.Player)
+		fmt.Println("Hand dealer: ", gs.Dealer.DealerString())
+		fmt.Println("Action? (H)it or (S)tand?")
+		fmt.Scanf("%s\n", &input)
 
-	// 	switch input {
-	// 	case "h":
-	// 		card, cards = draw(cards)
-	// 		player = append(player, card)
-	// 	}
-	// }
+		switch input {
+		case "h":
+			gs = Hit(gs)
+		case "s":
+			gs = Stand(gs)
+		default:
+			fmt.Println("Invalid option. Choose (h)it or (s)tand")
+		}
 
-	// if dealer.Score() <= 16 || (dealer.Score() == 17 && dealer.MinScore() < 17) {
-	// 	card, cards = draw(cards)
-	// 	dealer = append(dealer, card)
-	// }
+	}
 
-	// pScore, dScore := player.Score(), dealer.Score()
+	for gs.State == StateDealerTurn {
+		if gs.Dealer.Score() <= 16 || (gs.Dealer.Score() == 17 && gs.Dealer.MinScore() < 17) {
+			gs = Hit(gs)
+		} else {
+			gs = Stand(gs)
+		}
+	}
 
-	// fmt.Println("**Final hands**")
-	// fmt.Println("Player: ", player, "\nScore: ", pScore)
-	// fmt.Println("Dealer: ", dealer, "\nScore: ", dScore)
-
-	// switch {
-	// case pScore > 21:
-	// 	fmt.Println("You busted")
-	// case dScore > 21:
-	// 	fmt.Println("Dealer busted")
-	// case pScore > dScore:
-	// 	fmt.Println("You win!")
-	// case dScore > pScore:
-	// 	fmt.Println("You lose...")
-	// case dScore == pScore:
-	// 	fmt.Println("Draw!")
-	// }
+	gs = EndHand(gs)
 }
